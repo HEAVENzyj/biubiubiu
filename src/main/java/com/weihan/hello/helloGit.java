9da2dbabf797639016deae6914e95aeda3d23c63
HEAVEN <uandme19@163.com>
2019-04-03 15:03:13 +0800

    我的第二次提交

---
+++ src/main/java/com/weihan/hello/helloGit.java
@@ -0,0 +1,8 @@
+package com.weihan.hello;
+
+public class helloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git,I come from IDEA.Git你好,我来自IDEA.");
+        System.out.println("Welcome!Nice to meet you.");
+    }
+}
